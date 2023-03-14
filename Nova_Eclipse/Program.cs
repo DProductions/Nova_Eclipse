f0f1b02f8a81f1cbc7cc61d0c4e72acbcfad8094
Daniel Parks <adam.parks123@outlook.com>
2023-03-13 23:45:55 -0400

    // 2nd edit - 2nd push 13 March 2023 Corrected Loop ! Works as expected

--- Nova_Eclipse/Program.cs
+++ Nova_Eclipse/Program.cs
@@ -12,11 +12,14 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-namespace Nova_Eclipse
-{
-    internal class Program
-    {
 
+
+
+
+namespace Nova_Eclipse
+    {
+        internal class Program
+        {
             // 2nd edit - 2nd push 13 March 2023
             String myBDRMRoom, mybath1Room, myspareRM1Room, myStudyRoom, myHallwayRoom, myKitchenRoom, myDiningRMRoom, myLivingRMRoom, myBath2Room;
             String knifeWeapon, pistolWeapon, punchWeapon, kickWeapon;
@@ -24,102 +27,99 @@ namespace Nova_Eclipse
             String oneTreasure, fiveTreasure, tenTreasure, twentyTreasure;
             String flashlightItem, ropeItem, batteriesItem, watchItem;
 
-
-
-            
-
-
-
-        static string menueOption(int answer)
-        {
+            static void Main(string[] args)
+            {
                 bool isSelected = false;
-                while (!isSelected) 
-                { 
-                 Console.WriteLine("- - - - - Menu - - - - -\n" +
-                "Please make a selection\n" +
-                "   1. Display Rooms\n" +
-                "   2. Display Weapons\n" +
-                "   3. Display Potion\n" +
-                "   4. Display Treasure\n" +
-                "   5. Display Items\n" +
-                "   6. Exit-\n" +
-                "- - - - - - - - - - - -");
+                while (!isSelected)
+                {
+                    Console.WriteLine("- - - - - Menu - - - - -\n" +
+                   "Please make a selection\n" +
+                   "   1. Display Rooms\n" +
+                   "   2. Display Weapons\n" +
+                   "   3. Display Potion\n" +
+                   "   4. Display Treasure\n" +
+                   "   5. Display Items\n" +
+                   "   6. Exit-\n" +
+                   "- - - - - - - - - - - -");
+                    string end;
+                int answer = Convert.ToInt32(Console.ReadLine());
 
-                answer = Console.ReadLine();
-                string answer
-                    switch (answer, answer)
+                    switch ( answer)
                     {
-                    case 1:
-                        Console.WriteLine(
-                            "\n" +
-                            "                                     Back Door                   \n" +
-                            "________________________________________     __________________\n" +
-                            "|   My      |   Spare   |             |                        |\n" +
-                            "| Bedroom   |   Room    |   Study     |         Living Room    |\n" +
-                            "|_________  |_________  |   __________|                        |\n" +
-                            "| BTHRM1 |           Hallway                                   |\n" +
-                            "|_____________________ ________    __                          |\n" +
-                            "                      |             |                          |\n" +
-                            "                      |                           ______       |\n" +
-                            "                      |   Kitchen       Dining    |       |    |\n" +
-                            "                      |             |    Room     | BTHRM2|    |\n" +
-                            "                      |_____________|_____________|_______|    |\n" +
-                            "                                                       Front Door\n" +
-                            "\n" +
-                            "\n" +
-                            " 1. My Bedroom " +
-                            " 2. Bathroom 1 " +
-                            " 3. Spare Room " +
-                            " 4. Study " +
-                            " 5. Hallway " +
-                            " 6. Kitchen " +
-                            " 7. Dining Room " +
-                            " 8. Living Room " +
-                            " 9. Bathroom 2 "
-                            );
-                        break;
+                        case 1:
+                            Console.WriteLine(
+                                "\n" +
+                                "                                     Back Door                   \n" +
+                                "________________________________________     __________________\n" +
+                                "|   My      |   Spare   |             |                        |\n" +
+                                "| Bedroom   |   Room    |   Study     |         Living Room    |\n" +
+                                "|_________  |_________  |   __________|                        |\n" +
+                                "| BTHRM1 |           Hallway                                   |\n" +
+                                "|_____________________ ________    __                          |\n" +
+                                "                      |             |                          |\n" +
+                                "                      |                           ______       |\n" +
+                                "                      |   Kitchen       Dining    |       |    |\n" +
+                                "                      |             |    Room     | BTHRM2|    |\n" +
+                                "                      |_____________|_____________|_______|    |\n" +
+                                "                                                       Front Door\n" +
+                                "\n" +
+                                "\n" +
+                                " 1. My Bedroom " +
+                                " 2. Bathroom 1 " +
+                                " 3. Spare Room " +
+                                " 4. Study " +
+                                " 5. Hallway " +
+                                " 6. Kitchen " +
+                                " 7. Dining Room " +
+                                " 8. Living Room " +
+                                " 9. Bathroom 2 "
+                                );
 
-                    case 2:
-                        Console.WriteLine("____ Weapons____\n" +
-                        "   1 - Pistol\n" +
-                        "   2 - Knife\n" +
-                        "   3 - Punch\n" +
-                        "   4 - Kick\n");
-                        break;
+                            break;
 
-                    case 3:
-                         Console.WriteLine("____Potions____\n" +
-                        "   1 - Heal\n" +
-                        "   2 - Revive\n");
-                        break;
+                        case 2:
+                            Console.WriteLine("____ Weapons____\n" +
+                            "   1 - Pistol\n" +
+                            "   2 - Knife\n" +
+                            "   3 - Punch\n" +
+                            "   4 - Kick\n");
+                            break;
 
-                    case 4:
-                        Console.WriteLine("____Treasure____\n" +
-                        "   1 - $1.00\n" +
-                        "   2 - $5.00\n" +
-                        "   3 _ $10.00\n" +
-                        "   4 - $20.00\n");
-                        break;  
-           
-                    case 5:
-                        Console.WriteLine("____Items____\n" +
-                        "   1 - Flashlight\n" +
-                        "   2 - Rope\n" +
-                        "   3 _ Batteries\n" +
-                        "   4 - Watch\n");
-                        break;
+                        case 3:
+                            Console.WriteLine("____Potions____\n" +
+                           "   1 - Heal\n" +
+                           "   2 - Revive\n");
+                            break;
 
-                    case 6:
-                        Environment.Exit(0);
-                        break;
+                        case 4:
+                            Console.WriteLine("____Treasure____\n" +
+                            "   1 - $1.00\n" +
+                            "   2 - $5.00\n" +
+                            "   3 _ $10.00\n" +
+                            "   4 - $20.00\n");
+                            break;
 
-                    default:
-                        Console.WriteLine("Invalid Entry. Please make a selection. "
-                        break;
+                        case 5:
+                            Console.WriteLine("____Items____\n" +
+                            "   1 - Flashlight\n" +
+                            "   2 - Rope\n" +
+                            "   3 _ Batteries\n" +
+                            "   4 - Watch\n");
+
+                            break;
+
+                        case 6:
+                            isSelected = true;
+                            end = Console.ReadLine();
+                            break;
+
+                        default:
+                            Console.WriteLine("Invalid Entry. Please make a selection. ");
+
+                            break;
                     }
-                
+
                 }
-            
+            }
         }
     }
-}
